06efa432ad69a2d301972fa55061d14fb4f562bd
ySlenderG <opcn.gustavo@gmail.com>
2025-02-20 00:38:04 -0300

    atualizando o arquivo COMMIT_EDITMSG

--- excercicio 3.12.py
+++ excercicio 3.12.py
@@ -1,3 +1,4 @@
+
 distancia = float(input("Qual a distacia em km: "))
 velocidade_media = float(input("Qual a velocidade media em km/h: "))
 tempo = distancia / velocidade_media
--- excercicio 3.14.py
+++ excercicio 3.14.py
@@ -1,3 +1,5 @@
+#escreva um programa que calcule o tempo de uma viagem de carro.
+# Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
 km = float(input("Qual a distacia em km: "))
 dias = int(input("Quantos dias voce pretende viajar? "))
 valor = 60 * dias + 0.15 * km
